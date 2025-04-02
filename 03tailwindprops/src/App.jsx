09628693c51b0760c9fe1705ae87aab8fc821cf8
Ujjwal012003 <ujjwaal05@gmail.com>
2025-04-02 19:37:48 +0000

    props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-4xl font-bold text-purple-500 bg-green-500">
+      Bhai, Tailwind Chal Gaya!
+    </div>
+  );
+}
+export default App;
